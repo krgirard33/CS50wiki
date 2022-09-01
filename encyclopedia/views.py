57a44a28b6aebc26c49b358324a69f7784e8eb40
krgirard33 <ken.girard@gmail.com>
2022-09-01 13:13:57 -0500

    Added wiki url, wiki page, wiki view

--- encyclopedia/views.py
+++ encyclopedia/views.py
@@ -2,9 +2,17 @@ from django.shortcuts import render
 
 from . import util
 
+# Create Views Here 
 
 def index(request):
     return render(request, "encyclopedia/index.html", {
         "entries": util.list_entries()
     })
 
+def wiki(request, entry):
+    content = util.get_entry(entry)
+    
+    return render(request, "encyclopedia/wiki.html", {
+        "title": entry,
+        "content": content
+    })
