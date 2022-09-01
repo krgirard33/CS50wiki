dd4e800310da57343dfba0d9804df428c9fc734d
krgirard33 <ken.girard@gmail.com>
2022-09-01 15:20:40 -0500

    made search.html, views.search, search form action

--- encyclopedia/urls.py
+++ encyclopedia/urls.py
@@ -3,6 +3,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", views.index, name="index"),
-    path("wiki/<str:entry>", views.wiki, name="wiki")
+    path("", views.index, name="index"), # Gets home page
+    path("wiki/<str:entry>", views.wiki, name="wiki"), # Gets that wiki page
+    path("search", views.search, name="search") # If there is more than one found entry, it pulls up the search page
 ]
--- encyclopedia/views.py
+++ encyclopedia/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect 
 from markdown2 import Markdown 
 
 from . import util
@@ -17,3 +17,30 @@ def wiki(request, entry):
         "title": entry,
         "content": Markdown().convert(content)
     })
+
+def search(request):
+    query = request.GET.get("q", "")
+    entries = util.list_entries()
+    found_entries = [ 
+        valid_entry
+        for valid_entry in entries
+        if query.lower() in valid_entry.lower()
+    ]
+    
+    if query is None or query == "":
+        return render(
+            request, 
+            "encyclopedia/search.html",
+            {"found_entries": "",
+             "query": query
+            }
+        )
+        
+    if len(found_entries) == 1:
+        return redirect("wiki", found_entries[0])
+    
+    return render(
+        request, 
+        "encyclopedia/search.html",
+        {"found_entries": found_entries, "query": query}
+    )
